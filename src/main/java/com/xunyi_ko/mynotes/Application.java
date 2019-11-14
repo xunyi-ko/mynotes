4917d1278382ab3a63759026c49d62246f159a60
xunyi-ko <343885845@qq.com>
2019-11-14 14:34:29 +0800

    漏掉的东西

--- src/main/java/com/xunyi_ko/mynotes/Application.java
+++ src/main/java/com/xunyi_ko/mynotes/Application.java
@@ -1,5 +1,11 @@
 package com.xunyi_ko.mynotes;
 
-public class Application {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
